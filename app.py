694dff2703ffae82e1a0ed7478f2131b087f52ac
Nick Eccher <nicholas.eccher@owen.alumni.vanderbilt.edu>
2021-05-13 19:32:44 -0500

    Office hours fix push

--- app.py
+++ app.py
@@ -6,23 +6,23 @@ import scraping
 #Setting up Flask
 app = Flask(__name__)
 
-#Use flask_pymongo to set up mongo connection
+# Use flask_pymongo to set up mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
 #Define route
 @app.route("/")
 def index():
-    mars = mongo.db.mars.find_one()
-    return render_template("index.html", mars=mars)
+   mars = mongo.db.mars.find_one()
+   return render_template("index.html", mars=mars)
 
 #Set up Scraping route
 @app.route("/scrape")
 def scrape():
-    mars = mongo.db.mars
-    mars_data = scraping.scrape_all()
-    mars.update({}, mars_data, upsert=True)
-    return redirect('/', code=302)
+   mars = mongo.db.mars
+   mars_data = scraping.scrape_all()
+   mars.update({}, mars_data, upsert=True)
+   return redirect('/', code=302)
 
 #Tell Flask to run
 if __name__ == "__main__":
